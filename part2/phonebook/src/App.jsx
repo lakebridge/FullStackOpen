82a521d32436c4bc5ee233da86725ce652d8b545
leirsam <leirsam@gmail.com>
2026-05-24 19:24:05 +0100

    finished exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,9 +2,13 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState(["", ""]);
+  const [search, setSearch] = useState("");
 
   const addName = (event) => {
     event.preventDefault();
@@ -17,7 +21,13 @@ const App = () => {
       console.log("newName", newName);
       console.log("New name added:", newName[0], newName[1]);
     }
-    setPersons(persons.concat({ name: newName[0], number: newName[1] }));
+    setPersons(
+      persons.concat({
+        name: newName[0],
+        number: newName[1],
+        id: persons.length + 1,
+      }),
+    );
 
     // CLear the input fields
     setNewName(["", ""]);
@@ -29,7 +39,16 @@ const App = () => {
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>
+        filter shown with:{" "}
+        <input
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
+        />
+      </div>
+
+      <h2>Add a new</h2>
       <form>
         <div>
           name:{" "}
@@ -53,14 +72,27 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul style={{ padding: 0, margin: 0 }}>
-        {persons.map((person) => (
-          <li
-            style={{ listStyleType: "none", margin: 0, padding: 0 }}
-            key={person.name}
-          >
-            {person.name}: {person.number}
-          </li>
-        ))}
+        {search === ""
+          ? persons.map((person) => (
+              <li
+                style={{ listStyleType: "none", margin: 0, padding: 0 }}
+                key={person.name}
+              >
+                {person.name}: {person.number}
+              </li>
+            ))
+          : persons
+              .filter((person) =>
+                person.name.toLowerCase().includes(search.toLowerCase()),
+              )
+              .map((person) => (
+                <li
+                  style={{ listStyleType: "none", margin: 0, padding: 0 }}
+                  key={person.name}
+                >
+                  {person.name}: {person.number}
+                </li>
+              ))}
       </ul>
     </div>
   );
